f8a20ac10284d4e0ad36ab674dcdbd8b70860797
OleksandrZharkov <ozharkov.88@gmail.com>
2021-05-03 17:39:08 +0000

    update myiac

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ubuntu-server" {
   subnet_id = aws_subnet.x3iibits-subnet-public-1.id
   
   vpc_security_group_ids = [aws_security_group.x3iibits-all.id]
- 
+  key_name = "aws-keypair-myproject"
   tags = {
     "Name" = "ubuntu-server"
   }
