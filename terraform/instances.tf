30eeacf73079d6012c2121d70264d2984463baa7
OleksandrZharkov <ozharkov.88@gmail.com>
2021-05-03 11:12:09 +0000

    update iac

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,11 +1,11 @@
 
-resource "aws_instance" "EC2" {
+resource "aws_instance" "ubuntu-server" {
   ami           = "ami-0767046d1677be5a0"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.x3iibits-subnet-public-1.id
   vpc_security_group_ids = [aws_security_group.x3iibits-all.id]
   tags = {
-    "Name" = "Ubuntu Server"
+    "Name" = "ubuntu-server"
   }
 
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "ec2_endpoint" {
   description = "The address of the EC2 instance"
-  value       = aws_instance.EC2.public_ip
+  value       = aws_instance.ubuntu-server.public_ip
 }
