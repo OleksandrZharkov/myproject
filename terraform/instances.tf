ae6827c351a5f81a757d179d3e5eb21263fbc34b
OleksandrZharkov <ozharkov.88@gmail.com>
2021-05-03 11:34:35 +0000

    update myiac

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "ubuntu-server" {
-  ami           = "ami-0767046d1677be5a0"
+  ami           = "ami-009b16df9fcaac611"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.x3iibits-subnet-public-1.id
   vpc_security_group_ids = [aws_security_group.x3iibits-all.id]
